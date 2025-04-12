abfd2806cc1a0b3698565f223db45aeda4b0efa2
Ujjwal <ujjwalchoudhary065@gmail.com>
2025-04-12 22:59:21 +0530

    update xl size for button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -19,6 +19,7 @@ const buttonVariants = cva(
         sm: "px-4 py-1.5 text-sm",
         md: "px-6 py-2 text-base",
         lg: "px-8 py-3 text-lg",
+        xl: "px-14 py-2 text-base"
       },
     },
     defaultVariants: {
